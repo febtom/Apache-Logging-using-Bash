e75dfbbf80d44cdd52b564b49e55943072588876
febtom <73755233+febtom@users.noreply.github.com>
2021-12-01 11:50:27 +0530

    Create logging.sh

---
+++ logging.sh
@@ -0,0 +1,21 @@
+                                                                     logging.sh                                                                                 
+#!/bin/bash
+
+clear
+
+file="access.log" #file which holds access.log file contents
+
+echo "Extracting IP Addresses from Log File"
+echo "........BEGIN........."
+
+while true 
+
+do
+
+sudo awk '{print $1}' $file | uniq #lists unique IP addresses for each request 
+
+curl "https://ip-api.com">>ip.txt #get info from IP API
+
+sleep 30 #schelduler to run script every 30 seconds
+
+done
